9501397d94282d3369e976c4543a2808227dea6b
Szabolcs Dombi <cprogrammer1994@gmail.com>
2018-03-16 07:59:44 +0200

    version

--- misc/install_docker_on_ubuntu_16_04.sh
+++ misc/install_docker_on_ubuntu_16_04.sh
@@ -5,7 +5,7 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get update
 apt-get install -y docker-ce
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.19.0/docker-compose-`uname -s`-`uname -m` -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 docker --version
 docker-compose --version
